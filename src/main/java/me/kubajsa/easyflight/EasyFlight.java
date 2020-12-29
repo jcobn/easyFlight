4330685e058dc8f42e60a0f9639bd6780bea7061
Kubajsa <unconfigured@null.spigotmc.org>
2020-12-29 11:49:32 +0100

    Initial Commit

---
+++ src/main/java/me/kubajsa/easyflight/EasyFlight.java
@@ -0,0 +1,17 @@
+package me.kubajsa.easyflight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyFlight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
